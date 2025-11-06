e01fb57c6fb2a8e4292b9abfec38187ff51db79d
wildcomedian <joelgoodman12008@gmail.com>
2025-11-06 00:03:02 -0500

    * Update Function & Sub Functioning Buttons Nov 5
    * Update Card.java
    
    * Refactor Card class and fix image loading
    
    * Lotta Code
    
    * Update Function & Sub Functioning Buttons Nov 5
    
    ---------
    
    Co-authored-by: Fedor Menchukov <77518360+menchukf@users.noreply.github.com>

--- workspace/resources/GUI.java
+++ workspace/resources/GUI.java
@@ -5,7 +5,6 @@
  */
 package resources;
 import javax.imageio.ImageIO;
-import javax.print.attribute.AttributeSetUtilities;
 import javax.swing.*;
 
 import java.awt.*;
@@ -17,17 +16,31 @@ import java.awt.event.MouseMotionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Stack;
 
 
 
 public class GUI extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
 
-	Blackjack game;
+	private Blackjack game;
+
+	private JButton hitOption;
+	private JButton standOption;
+	private JButton doubleOption;
+	
+	private JLabel playerBankDisplay;
+	private JLabel potDisplay;
+
+	private JPanel dealerArea;
+	private JPanel deckArea;
+	private JPanel playerArea;
+
    	public GUI(Blackjack game){
 		this.game = game;
-
-    	//Create and set up the window.
+		game.newRound(); //Starts a round of BlackJack
+		
+		//Create and set up the window.
        	setTitle("Blackjack");
        	setSize(900,580);
        	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,7 +62,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	   	setLayout(layout);
 
 	   	//top left
-	   	JPanel dealerArea = new JPanel(new BorderLayout());
+	   	dealerArea = new JPanel(new BorderLayout());
 	   	dealerArea.setOpaque(false);
 	   	dealerArea.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
 	   	c.gridx = 0;
@@ -59,8 +72,11 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	   	c.weighty = 0.5;
 	   	add(dealerArea, c);
 
+		//ADD TO UPDATE
+		dealerArea.add( drawPile( listToStack(game.getDealerHand()), 20 ) );
+
 	   	//top right
-	   	JPanel deckArea = new JPanel(new BorderLayout());
+	   	deckArea = new JPanel(new BorderLayout());
 	   	deckArea.setOpaque(false);
 	   	deckArea.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
 	   	c.gridx = 2;
@@ -83,9 +99,10 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	   	c.weighty = 0.5;
 	   	add(bankrollArea, c);
 
-	   	JLabel potDisplay = new JLabel(Integer.toString(game.getPotAmount()));
+		
+	   	potDisplay = new JLabel(Integer.toString(game.getPotAmount())); //ADD TO UPDATE
 
-		//Style
+		//Style (ADD TO UPDATE)
 	   	potDisplay.setOpaque(true);
 		potDisplay.setFont(new Font(Font.DIALOG, Font.BOLD,30));
 	   	potDisplay.setBackground(new Color(50, 204, 0));
@@ -99,7 +116,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 
 	   	bankrollArea.add(potDisplay, BorderLayout.NORTH);
 
-	   	JLabel playerBankDisplay = new JLabel(Integer.toString(game.getPlayerBankroll()));
+	   	playerBankDisplay = new JLabel(Integer.toString(game.getPlayerBankroll())); //ADD TO UPDATE
 
 		//Style
 	   	playerBankDisplay.setOpaque(true);
@@ -116,7 +133,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	   	bankrollArea.add(playerBankDisplay, BorderLayout.SOUTH);
 
 	   	//bottom middle
-	   	JPanel playerArea = new JPanel(new BorderLayout());
+	   	playerArea = new JPanel(new BorderLayout());
 	   	playerArea.setOpaque(false);
 	   	playerArea.setBorder(BorderFactory.createLineBorder(Color.GREEN, 3));
 	   	c.gridx = 1;
@@ -124,7 +141,9 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	   	c.gridwidth = 1;
 	   	c.weightx = 0.5;
 	   	c.weighty = 0.5;
-	   	add(playerArea, c);
+	   	this.add(playerArea, c);
+
+		playerArea.add( drawPile( listToStack(game.getPlayerHand()), 20 ) );
 
 	   	//bottom right
 	   	JPanel optionArea = new JPanel(new GridLayout(3, 1));
@@ -135,32 +154,57 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	   	c.gridwidth = 1;
 	   	c.weightx = 0.3;
 	   	c.weighty = 0.5;
-	   
 	   	add(optionArea, c);
 
 		//Buttons
-		optionArea.setOpaque(false);
-		JButton hitOption = new JButton("HIT");
+		hitOption = new JButton("HIT");
 		hitOption.setFont(new Font("SansSerif", Font.BOLD, 20));
-		hitOption.setBackground(new Color(102, 204, 0));
+		hitOption.setBackground(new Color(102, 204, 0, 80));
 		hitOption.setForeground(Color.WHITE);
+		hitOption.addMouseListener(this);
 	   	optionArea.add(hitOption, BorderLayout.NORTH);
 	   
-	   	JButton standOption = new JButton("STAND");
+	   	standOption = new JButton("STAND");
 		standOption.setFont(new Font("SansSerif", Font.BOLD, 20));
-		standOption.setBackground(new Color(0, 102, 204));
+		standOption.setBackground(new Color(0, 102, 204, 80));
 		standOption.setForeground(Color.WHITE);
+		standOption.addMouseListener(this);
 	   	optionArea.add(standOption, BorderLayout.CENTER);
 	   
-	   	JButton doubleOption = new JButton("DOUBLE");
+	   	doubleOption = new JButton("DOUBLE");
 		doubleOption.setFont(new Font("SansSerif", Font.BOLD, 20));
-		doubleOption.setBackground(new Color(204, 102, 0));
+		doubleOption.setBackground(new Color(204, 102, 0, 80));
 		doubleOption.setForeground(Color.WHITE);
+		doubleOption.addMouseListener(this);
 	   	optionArea.add(doubleOption, BorderLayout.SOUTH);
 
     	
 		this.setVisible(true);
     }
+
+	private void update() {
+		//Update Player's Hand
+		playerArea.removeAll();
+		playerArea.add( drawPile( listToStack(game.getPlayerHand()), 20 ) );
+
+		//Update Dealer's Hand
+		dealerArea.removeAll();
+		dealerArea.add( drawPile( listToStack(game.getDealerHand()), 20 ) );
+
+		//Pot Amount Label Display
+		potDisplay.setText(Integer.toString(game.getPotAmount()));
+
+		//Player Bankroll Label Display
+		playerBankDisplay.setText(Integer.toString(game.getPlayerBankroll()));
+
+		//Update Button Opacity
+		hitOption.setBackground(new Color(102, 204, 0, 80));
+		standOption.setBackground(new Color(0, 102, 204,80));
+		doubleOption.setBackground(new Color(204, 102, 0, 80));
+
+		this.revalidate();
+		this.repaint();
+	}
 
 	public JLayeredPane drawPile(Stack<Card> cardStack, int overlap){
 			Object[] cards = cardStack.toArray();
@@ -178,7 +222,14 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 			return pile;
 	   }
 
-	   
+	private Stack<Card> listToStack(ArrayList<Card> list) {
+		Stack<Card> result = new Stack<Card>();
+		for (Card card : list){
+			result.push(card);
+		}
+		return result;
+	}
+
 
 
 	@Override
@@ -202,18 +253,35 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
 	}
-
+	
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		if (arg0.getSource() instanceof JButton) {
+			update();
+		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+
+		if (arg0.getSource() instanceof JButton){
+			JButton thisButton = (JButton)(arg0.getSource());
+			if (thisButton == hitOption) {
+				game.playerHit();
+				System.out.println("Player picked HIT");
+			}
+			else if (thisButton == standOption) {
+				game.playerStand();
+				System.out.println("Player picked STAND");
+			}
+			else if (thisButton == doubleOption) {
+				game.playerDouble();
+				System.out.println("Player picked DOUBLE");
+			}
+		}
+
+		update();
 		
 	}
 
